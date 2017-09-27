bd34047774ad309f617c5b7ca93960300e086551
BlaneTorregrossa <blanet97@cox.net>
2017-09-27 17:58:20 -0500

    started on texture work

---
+++ 5.Textures/main.cpp
@@ -0,0 +1,15 @@
+#include "_5_TexturesApp.h"
+
+int main() {
+	
+	// allocation
+	auto app = new _5_TexturesApp();
+
+	// initialise and loop
+	app->run("AIE", 1280, 720, false);
+
+	// deallocation
+	delete app;
+
+	return 0;
+}
