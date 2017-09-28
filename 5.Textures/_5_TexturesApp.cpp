d1248e2ba747277452c27f4c3cbd9e8d2c47a809
BlaneTorregrossa <blanet97@cox.net>
2017-09-28 18:00:57 -0500

    Fixed issue with debug and build

--- 5.Textures/_5_TexturesApp.cpp
+++ 5.Textures/_5_TexturesApp.cpp
@@ -1,12 +1,13 @@
 #include "_5_TexturesApp.h"
 #include "Gizmos.h"
 #include "Input.h"
+
+#define STB_IMAGE_IMPLEMENTATION
+#include<stb_image.h>
+#include <vector>
 #include <glm/glm.hpp>
 #include <glm/ext.hpp>
 #include <gl_core_4_4.h>
-
-#define STB_IMAGE_IMPLEMENTATION // Needs to be used for the stb includes to work
-#include <stb_image.h>
 
 using glm::vec3;
 using glm::vec4;
@@ -22,7 +23,7 @@ _5_TexturesApp::~_5_TexturesApp() {
 }
 
 bool _5_TexturesApp::startup() {
-	
+
 	setBackgroundColour(0.25f, 0.25f, 0.25f);
 
 	// initialise gizmo primitive counts
@@ -33,32 +34,31 @@ bool _5_TexturesApp::startup() {
 	m_projectionMatrix = glm::perspective(glm::pi<float>() * 0.25f, 16.0f / 9.0f, 0.1f, 1000.0f);
 
 	int imageWidth = 0, imageHeight = 0, imageFormat = 0;
-	unsigned char* data = stbi_load("./dependencies/crate.png", &imageWidth, &imageHeight, &imageFormat, STBI_default);
-
-	glGenTextures(1, &m_texture); // Generate an OGL texture handle
-	glBindTexture(GL_TEXTURE_2D, m_texture); // bind texture to the correct slot for dimension (2-D)
-	glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB, imageWidth, imageHeight, 0, GL_RGB, GL_UNSIGNED_BYTE, data); // specifying data for the texture (width, height, format, variable type)
-	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR); 
+	unsigned char* data = stbi_load("./textures/crate.png", &imageWidth, &imageHeight, &imageFormat, STBI_default);
+	glGenTextures(1, &m_texture);	// used to generate many texture handles at once
+	glBindTexture(GL_TEXTURE_2D, m_texture);	// bind the textures to the correct slot
+	glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB, imageWidth, imageHeight, 0, GL_RGB, GL_UNSIGNED_BYTE, data);	// speciy the data for the texture (format, resolution and variable type). 
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
 	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
+	stbi_image_free(data);	// free the loaded data of the texture
 
-	stbi_image_free(data); // frees the texture data
+#pragma region load shader
+	const char* vsSource =   "#version 410\n \
+							 layout(location=0) in vec4 position; \
+							 layout(location=1) in vec2 texCoord; \
+							 out vec2 vTexCoord; \
+							 uniform mat4 projectionViewWorldMatrix; \
+							 void main() { \
+							 vTexCoord = texCoord; \
+							 gl_Position = projectionViewWorldMatrix * position; \
+							 }";
 
-	const char* vsSource = "version 410\n \
-							layout(loacation=0) in vec4 position; \
-							layout(loacation=1) in vec2 texCoord; \
-							out vec2 vTexCoord; \
-							uniform mat4 projectionViewWorldMatrix; \
-							void main() { \
-							vTexCoord = texCoord; \
-							gl_Position = projectionViewWorldMatrix * position; \
-							}";
-
-	const char* fsSource = "version 410\n \
+	const char* fsSource =  "#version 410\n \
 							in vec2 vTexCoord; \
 							out vec4 fragColor; \
 							uniform sampler2D diffuse; \
-							void main () { \
-							fragColor = texture(diffuse, vTexCoord); \
+							void main() { \
+							fragColor = texture(diffuse,vTexCoord); \
 							}";
 
 	unsigned int vertexShader = glCreateShader(GL_VERTEX_SHADER);
@@ -69,15 +69,20 @@ bool _5_TexturesApp::startup() {
 	glShaderSource(fragmentShader, 1, (const char**)&fsSource, 0);
 	glCompileShader(fragmentShader);
 
+#pragma region attach shader
 	m_program = glCreateProgram();
 	glAttachShader(m_program, vertexShader);
 	glAttachShader(m_program, fragmentShader);
 	glLinkProgram(m_program);
+#pragma endregion
+	// seperating endregions
+#pragma endregion
 
 	glDeleteShader(vertexShader);
 	glDeleteShader(fragmentShader);
 
-	float vertexData[] =
+#pragma region plane
+	float vertexData[] = 
 	{
 		-5, 0, 5, 1, 0, 1,
 		5, 0, 5, 1, 1, 1,
@@ -85,18 +90,20 @@ bool _5_TexturesApp::startup() {
 		-5, 0, -5, 1, 0, 0,
 	};
 
-	unsigned int indexData[] =
+	unsigned int indexData[] = 
 	{
 		0, 1, 2,
 		0, 2, 3,
 	};
+#pragma endregion
 
+#pragma region createBuffers
 	glGenVertexArrays(1, &m_vao);
 	glBindVertexArray(m_vao);
 
 	glGenBuffers(1, &m_vbo);
 	glBindBuffer(GL_ARRAY_BUFFER, m_vbo);
-	glBufferData(GL_ARRAY_BUFFER, sizeof(float) * 6 * 4, vertexData, GL_STATIC_DRAW);
+	glBufferData(GL_ARRAY_BUFFER, sizeof(float) * 6 * 4 , vertexData, GL_STATIC_DRAW);
 
 	glGenBuffers(1, &m_ibo);
 	glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, m_ibo);
@@ -104,27 +111,31 @@ bool _5_TexturesApp::startup() {
 
 	glEnableVertexAttribArray(0);
 	glVertexAttribPointer(0, 4, GL_FLOAT, GL_FALSE, sizeof(float) * 6, 0);
-
 	glEnableVertexAttribArray(1);
-	glVertexAttribPointer(1, 4, GL_FLOAT, GL_FALSE, sizeof(float) * 6, ((char*)0) + 16);
-
+	glVertexAttribPointer(1, 4, GL_FLOAT, GL_FALSE, sizeof(float) * 6, ((char*)0)+16);
+#pragma endregion
+	//unbind mesh
 	glBindVertexArray(0);
 	glBindBuffer(GL_ARRAY_BUFFER, 0);
 	glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, 0);
 
-	glUseProgram(m_program);	
-	
+	// for drawing
+	glUseProgram(m_program); // use texture program
+
+	//camera bind
 	int loc = glGetUniformLocation(m_program, "projectionViewWorldMatrix");
 	glUniformMatrix4fv(loc, 1, GL_FALSE, &m_projectionMatrix[0][0]);
 
+	// set texture slot
 	glActiveTexture(GL_TEXTURE0);
 	glBindTexture(GL_TEXTURE_2D, m_texture);
 
 	loc = glGetUniformLocation(m_program, "diffuse");
 	glUniform1i(loc, 0);
 
+	// draws
 	glBindVertexArray(m_vao);
-	glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, nullptr);
+	glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);
 
 	return true;
 }
@@ -144,11 +155,11 @@ void _5_TexturesApp::update(float deltaTime) {
 	vec4 black(0, 0, 0, 1);
 	for (int i = 0; i < 21; ++i) {
 		Gizmos::addLine(vec3(-10 + i, 0, 10),
-						vec3(-10 + i, 0, -10),
-						i == 10 ? white : black);
+			vec3(-10 + i, 0, -10),
+			i == 10 ? white : black);
 		Gizmos::addLine(vec3(10, 0, -10 + i),
-						vec3(-10, 0, -10 + i),
-						i == 10 ? white : black);
+			vec3(-10, 0, -10 + i),
+			i == 10 ? white : black);
 	}
 
 	// add a transform so that we can see the axis
