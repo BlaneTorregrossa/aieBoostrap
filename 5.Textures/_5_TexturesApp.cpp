a3d1f60adbbde6f9fcf5963cb8f29334341d0e22
BlaneTorregrossa <blanet97@cox.net>
2017-09-29 14:20:35 -0500

    9/29/2017 2:20 P.M.

--- 5.Textures/_5_TexturesApp.cpp
+++ 5.Textures/_5_TexturesApp.cpp
@@ -1,18 +1,20 @@
 #include "_5_TexturesApp.h"
 #include "Gizmos.h"
 #include "Input.h"
-
-#define STB_IMAGE_IMPLEMENTATION
-#include<stb_image.h>
 #include <vector>
 #include <glm/glm.hpp>
 #include <glm/ext.hpp>
 #include <gl_core_4_4.h>
 
+#define STB_IMAGE_IMPLEMENTATION
+#include<stb_image.h>
+
 using glm::vec3;
 using glm::vec4;
 using glm::mat4;
 using aie::Gizmos;
+
+using namespace glm;
 
 _5_TexturesApp::_5_TexturesApp() {
 
@@ -30,20 +32,23 @@ bool _5_TexturesApp::startup() {
 	Gizmos::create(10000, 10000, 10000, 10000);
 
 	// create simple camera transforms
-	m_viewMatrix = glm::lookAt(vec3(10), vec3(0), vec3(0, 1, 0));
+	m_viewMatrix = glm::lookAt(vec3(0,0,10), vec3(0), vec3(0, 1, 0));
 	m_projectionMatrix = glm::perspective(glm::pi<float>() * 0.25f, 16.0f / 9.0f, 0.1f, 1000.0f);
 
-	int imageWidth = 0, imageHeight = 0, imageFormat = 0;
+	int imageWidth, imageHeight, imageFormat;
 	unsigned char* data = stbi_load("./textures/crate.png", &imageWidth, &imageHeight, &imageFormat, STBI_default);
+
 	glGenTextures(1, &m_texture);	// used to generate many texture handles at once
 	glBindTexture(GL_TEXTURE_2D, m_texture);	// bind the textures to the correct slot
 	glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB, imageWidth, imageHeight, 0, GL_RGB, GL_UNSIGNED_BYTE, data);	// speciy the data for the texture (format, resolution and variable type). 
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR_MIPMAP_LINEAR);
 	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
-	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
+
 	stbi_image_free(data);	// free the loaded data of the texture
 
+
 #pragma region load shader
-	const char* vsSource =   "#version 410\n \
+	const char* vsSource = "#version 410\n \
 							 layout(location=0) in vec4 position; \
 							 layout(location=1) in vec2 texCoord; \
 							 out vec2 vTexCoord; \
@@ -53,7 +58,7 @@ bool _5_TexturesApp::startup() {
 							 gl_Position = projectionViewWorldMatrix * position; \
 							 }";
 
-	const char* fsSource =  "#version 410\n \
+	const char* fsSource = "#version 410\n \
 							in vec2 vTexCoord; \
 							out vec4 fragColor; \
 							uniform sampler2D diffuse; \
@@ -62,10 +67,9 @@ bool _5_TexturesApp::startup() {
 							}";
 
 	unsigned int vertexShader = glCreateShader(GL_VERTEX_SHADER);
+	unsigned int fragmentShader = glCreateShader(GL_FRAGMENT_SHADER);
 	glShaderSource(vertexShader, 1, (const char**)&vsSource, 0);
 	glCompileShader(vertexShader);
-
-	unsigned int fragmentShader = glCreateShader(GL_FRAGMENT_SHADER);
 	glShaderSource(fragmentShader, 1, (const char**)&fsSource, 0);
 	glCompileShader(fragmentShader);
 
@@ -75,22 +79,22 @@ bool _5_TexturesApp::startup() {
 	glAttachShader(m_program, fragmentShader);
 	glLinkProgram(m_program);
 #pragma endregion
-	// seperating endregions
-#pragma endregion
 
 	glDeleteShader(vertexShader);
 	glDeleteShader(fragmentShader);
+#pragma endregion
+
 
 #pragma region plane
-	float vertexData[] = 
+	float vertexData[] =
 	{
-		-5, 0, 5, 1, 0, 1,
-		5, 0, 5, 1, 1, 1,
-		5, 0, -5, 1, 1, 0,
-		-5, 0, -5, 1, 0, 0,
+		-1, 0, 1, 1,		 0, 1,
+		1, 0, 1, 1,			1, 1,
+		1, 0, -1, 1,		1, 0,
+		-1, 0, -1, 1,		0, 0,
 	};
 
-	unsigned int indexData[] = 
+	unsigned int indexData[] =
 	{
 		0, 1, 2,
 		0, 2, 3,
@@ -103,39 +107,23 @@ bool _5_TexturesApp::startup() {
 
 	glGenBuffers(1, &m_vbo);
 	glBindBuffer(GL_ARRAY_BUFFER, m_vbo);
-	glBufferData(GL_ARRAY_BUFFER, sizeof(float) * 6 * 4 , vertexData, GL_STATIC_DRAW);
+	glBufferData(GL_ARRAY_BUFFER, sizeof(vertexData), vertexData, GL_STATIC_DRAW);
 
 	glGenBuffers(1, &m_ibo);
 	glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, m_ibo);
-	glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(unsigned int) * 6, indexData, GL_STATIC_DRAW);
+	glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indexData), indexData, GL_STATIC_DRAW);
 
 	glEnableVertexAttribArray(0);
 	glVertexAttribPointer(0, 4, GL_FLOAT, GL_FALSE, sizeof(float) * 6, 0);
+
 	glEnableVertexAttribArray(1);
-	glVertexAttribPointer(1, 4, GL_FLOAT, GL_FALSE, sizeof(float) * 6, ((char*)0)+16);
-#pragma endregion
-	//unbind mesh
+	glVertexAttribPointer(1, 2, GL_FLOAT, GL_FALSE, sizeof(float) * 6, (void*)(6 * sizeof(float)));
+
+	//cleanup
 	glBindVertexArray(0);
 	glBindBuffer(GL_ARRAY_BUFFER, 0);
 	glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, 0);
-
-	// for drawing
-	glUseProgram(m_program); // use texture program
-
-	//camera bind
-	int loc = glGetUniformLocation(m_program, "projectionViewWorldMatrix");
-	glUniformMatrix4fv(loc, 1, GL_FALSE, &m_projectionMatrix[0][0]);
-
-	// set texture slot
-	glActiveTexture(GL_TEXTURE0);
-	glBindTexture(GL_TEXTURE_2D, m_texture);
-
-	loc = glGetUniformLocation(m_program, "diffuse");
-	glUniform1i(loc, 0);
-
-	// draws
-	glBindVertexArray(m_vao);
-	glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);
+#pragma endregion
 
 	return true;
 }
@@ -147,25 +135,29 @@ void _5_TexturesApp::shutdown() {
 
 void _5_TexturesApp::update(float deltaTime) {
 
-	// wipe the gizmos clean for this frame
-	Gizmos::clear();
+	//// wipe the gizmos clean for this frame
+	//Gizmos::clear();
 
-	// draw a simple grid with gizmos
-	vec4 white(1);
-	vec4 black(0, 0, 0, 1);
-	for (int i = 0; i < 21; ++i) {
-		Gizmos::addLine(vec3(-10 + i, 0, 10),
-			vec3(-10 + i, 0, -10),
-			i == 10 ? white : black);
-		Gizmos::addLine(vec3(10, 0, -10 + i),
-			vec3(-10, 0, -10 + i),
-			i == 10 ? white : black);
-	}
+	//// for camera to move and rotate around the grid
+	//float time = getTime();
+	//m_viewMatrix = glm::lookAt(vec3(glm::sin(time) * 20, 5, glm::cos(time) * 15), vec3(0), vec3(0, 1, 0));
 
-	// add a transform so that we can see the axis
-	Gizmos::addTransform(mat4(1));
+	//// draw a simple grid with gizmos
+	//vec4 white(1);
+	//vec4 black(0, 0, 0, 1);
+	//for (int i = 0; i < 21; ++i) {
+	//	Gizmos::addLine(vec3(-10 + i, 0, 10),
+	//		vec3(-10 + i, 0, -10),
+	//		i == 10 ? white : black);
+	//	Gizmos::addLine(vec3(10, 0, -10 + i),
+	//		vec3(-10, 0, -10 + i),
+	//		i == 10 ? white : black);
+	//}
 
-	// quit if we press escape
+	//// add a transform so that we can see the axis
+	//Gizmos::addTransform(mat4(1));
+
+	//// quit if we press escape
 	aie::Input* input = aie::Input::getInstance();
 
 	if (input->isKeyDown(aie::INPUT_KEY_ESCAPE))
@@ -177,8 +169,24 @@ void _5_TexturesApp::draw() {
 	// wipe the screen to the background colour
 	clearScreen();
 
-	// update perspective based on screen size
-	m_projectionMatrix = glm::perspective(glm::pi<float>() * 0.25f, getWindowWidth() / (float)getWindowHeight(), 0.1f, 1000.0f);
+	// for drawing
+	glUseProgram(m_program); // use texture program
+
+	// camera bind
+	int loc = glGetUniformLocation(m_program, "projectionViewWorldMatrix");
+	glUniformMatrix4fv(loc, 1, GL_FALSE, &m_projectionMatrix[0][0]);
+ 
+	loc = glGetUniformLocation(m_program, "diffuse");
+	glUniform1i(loc, 0);
+
+	// set texture slot
+	glActiveTexture(GL_TEXTURE0);
+	glBindTexture(GL_TEXTURE_2D, m_texture);
+
+	// draws
+	glBindVertexArray(m_vao);
+	glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, nullptr);
+	glBindVertexArray(0);
 
 	Gizmos::draw(m_projectionMatrix * m_viewMatrix);
 }
