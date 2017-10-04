ce1b0838d19df678a44602a02cd172228956138f
BlaneTorregrossa <blanet97@cox.net>
2017-10-04 13:40:47 -0500

    class for camera added

---
+++ 2.CamerasAndProjections/main.cpp
@@ -0,0 +1,15 @@
+#include "_2_CamerasAndProjectionsApp.h"
+
+int main() {
+	
+	// allocation
+	auto app = new _2_CamerasAndProjectionsApp();
+
+	// initialise and loop
+	app->run("AIE", 1280, 720, false);
+
+	// deallocation
+	delete app;
+
+	return 0;
+}
